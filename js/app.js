c1c5fc235429b9c4d66f60dc9db72f6fa2af0242
David Taylor <taylor.david.ray@gmail.com>
2015-09-26 23:19:44 -0700

    able to play 2 player tic tac toe. now need to implement ai

--- js/app.js
+++ js/app.js
@@ -7,12 +7,14 @@ console.log("app is up")
 $(function(){
 
     var Board = {
+        
         init: function() {
           this.board           = ["", "", "", "", "", "", "", "", ""] // [0] -> top_left, [8] -> bottom_right
           this.occupied_spaces = 0
           console.log("Board.init invoked")                    
         },
         // have is_winner return either false or else an array of the winning indices, this array is truthy
+        
         is_winner: function(color) {
           console.log("Board.is_winner invoked")
           var row;
@@ -64,10 +66,12 @@ $(function(){
           if (match) { return indices } //true }    
           return false;
         },
+        
         is_full: function() {
           if (this.occupied_spaces >= 9) { return true }
           return false;
         },
+        
         mark: function(index, color) {
           console.log("Board.mark invoked with "+index+", "+color)
           console.log(this.board);
@@ -78,107 +82,138 @@ $(function(){
           }
           return false
         },
+        
         is_valid: function(index) {
           return this.is_equal(index,"")
         },
+        
         set_cell: function(index,color) {
           this.board[index] = color
           console.log(this.board)
         },
+        
         get_cell: function(index) {
           return this.board[index]
         },
+        
         is_equal: function(index,value) {
           return this.get_cell(index) === value
         },
+        
         is_not_equal: function(index,value) {
-          return !this.is_equal(index,value)
+          return this.get_cell(index) !== value
         }      
     };
 
 
     var View = {
+        
         init: function() {
           console.log("View.init invoked")
           this.is_first_time = true
-          //this.quote = $("#quote")  // change this to View later 
-          //this.render(Controller.getQuote()) // change this to View later
-          
-          $(document).ready(function() {                      
+          this.reset()
 
-            $('.square').off('click').on('click', function() {
-
-              var id_index = this.id.substring(1,2)
-              // call GameController.mark with this and id_index
-              // id_index could be extracted from this, but i think this is a view concern
-              //View.renderSquare(this)              
-              GameController.mark(this,id_index)
-
-            }) 
-            if (this.is_first_time) { alert("Batman goes first!!") }
-            this.is_first_time = false
-          }) // document.ready
         },
-        clear: function() {
-          var grids = $('.square');
-          console.log("grids")
-          console.log(grids) // this shouldnt be called for just one click event
-        },      
-        render_square: function(e,attr,image) { 
-          console.log("render invoked")              
-          $(e).empty(); // referring to the jQuery this object
-          $(e).append(View.template(attr,image));
-        },
-        render_winner: function(winner_ary) {
-          // render winner indices by fading others
-          [0,1,2,3,4,5,6,7,8].forEach( function(index) {
-            if (winner_ary.indexOf(index) === -1) {
-              console.log("DRT "+index)
-              $("#e"+index+" div").addClass("fade")
-            }
+
+        // each number represents a unique id in index.html which corresponds to each square
+        forEach_square: function(callback) {
+          [0,1,2,3,4,5,6,7,8].forEach( function(elem) {
+            callback(elem)
           })
         },
-        // this should be private, not sure how
-        // if image is not given, then it will be an empty square returned
-        template: function(attr,image) {
-          var template = "";
-          template += "<div class='content'>"
-          if (image) { template +=   "<img "+attr+" src='"+image+"'>" }
-          template += "</div>"
-          console.log(template)
-          return template
-        }            
+
+        reset: function() {
+
+          var template = function(index) {
+            var html_template = ""
+            html_template += "<div id='e"+index+"' class='square'>"
+            html_template += "  <div class='content'>"
+            html_template += "  </div>"
+            html_template += "</div>"
+            return html_template
+          }
+
+          // Note that the order of the following code is important, building the DOM must happen
+          // prior to setting up the click handler. I believe there is another way to accomplish this.
+          var game_board = $('#game_board');
+          game_board.empty()
+          this.forEach_square(function (elem) {
+            game_board.append(template(elem))
+          })
+          console.log("game_board")          
+          console.log(game_board)
+          $(document).ready(function() {                            
+            $('.square').off('click').on('click', function() {
+              // id_index could be extracted from jQuery object 'this' downstream, but i think this is a view concern  
+              var id_index = this.id.substring(1,2)        
+              GameController.mark(this,id_index)
+            })                     
+          }) // document.ready 
+
+          if (this.is_first_time) { alert("Batman goes first!!") }
+            console.log("this is first time = "+this.is_first_time)  
+            this.is_first_time = false
+            console.log("this is first time = "+this.is_first_time)
+        },  
+
+        render_square: function(e,attr,image) { 
+          console.log("render invoked")         
+
+          // if image is not given, then an empty square will be returned
+          var template = function(attr,image) {
+            var html_template = "";
+            html_template += "<div class='content'>"
+            if (image) { html_template +=   "<img "+attr+" src='"+image+"'>" }
+            html_template += "</div>"
+            return html_template
+          }           
+          
+          $(e).empty(); // referring to the jQuery this object
+          $(e).append(template(attr,image));
+        },
+
+        render_winner: function(winner_ary) {
+          // render winner indices by fading others
+          this.forEach_square( function(elem) {
+            if(winner_ary.indexOf(elem) === -1) {
+              $("#e"+elem+" div").addClass("fade")
+          }
+          })
+        }
+             
     };
 
     var Game = {
+       
        current_player: function() {
           return this.pieces[this.current_player_index]
         },
+       
        current_image: function() {
           return this.images[this.current_player_index]
        },
+       
        current_attr: function() {
           return this.attr[this.current_player_index]
        },
+       
        next_player: function() {
          this.current_player_index = (this.current_player_index + 1) % 2
        },      
+       
        init: function() { 
-         this.pieces = ["B", "J"] // Batman, Joker
+         this.pieces = ["Batman", "Joker"] // Batman, Joker
          // not sure if I should be storing image and attr inside of game. more of a view thing unless it 
          // can be changed, selected by the user
          this.images = ["images/batman_logo_final.png", "images/aa6e8d0ba1ee3a0fea3df7c7d5b00f6b.jpg"]
          this.attr   = ["class='circle'", ""] // batman is a circle, jocker is a square
          this.current_player_index = 0
          Board.init()
-         //Controller.setQuoteIsReady(false);
-         //QuoteService.init()         
-         //Controller.setRenderAfterPrefetch(true);
-         //Controller.getQuoteFromService(0)
        }
     };
 
     var GameController = {
+      
       mark: function(e, index) {
         console.log("GameController.mark invoked")
         var current_player = Game.current_player()
@@ -202,12 +237,15 @@ $(function(){
               Game.next_player()
           }
           if (player_wins || draw) {
-            GameController.init()
-            View.clear()
+            Game.init()
+            Board.init()
+            View.reset()
+            //GameController.init()            
           }
           console.log("Here")
         }
       },
+      
       init: function() { 
         Game.init()
         Board.init()
