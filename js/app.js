5a70a2ed04706437fca1fa5dd3804172f43f5ae0
David Taylor <taylor.david.ray@gmail.com>
2015-09-27 14:48:25 -0700

    refactor code. remove old comments

--- js/app.js
+++ js/app.js
@@ -14,8 +14,8 @@ $(function(){
           this.occupied_spaces = 0
           console.log("Board.init invoked")                    
         },
+
         // have is_winner return either false or else an array of the winning indices, this array is truthy
-        
         is_winner: function(color) {
           console.log("Board.is_winner invoked")
           var row;
@@ -23,14 +23,11 @@ $(function(){
           var match;
           var indices;
 
-          //var offset = function(row, col) { return row*3+col }  
-
           // check rows
           for (row=0; row<3; row+=1) {
             match = true;
             indices = []
             for (col=0; col<3; col+=1) {
-              //index = offset(row,col)
               if (this.is_not_equal(row,col,color)) { match = false }
               else indices.push({row: row, column: col})
             }
@@ -41,7 +38,6 @@ $(function(){
             match = true;
             indices = []
             for (row=0; row<3; row+=1) {
-              //index = offset(row,col)
               if (this.is_not_equal(row,col,color)) { match = false }
               else indices.push({row: row, column: col})
             }
@@ -52,7 +48,6 @@ $(function(){
           indices = []
           for (row=0; row<3; row+=1) {
             col=row;
-            //index = offset(row,col)
             if (this.is_not_equal(row,col,color)) { match = false }
             else indices.push({row: row, column: col})
           }
@@ -62,7 +57,6 @@ $(function(){
           indices = []
           for (row=0; row<3; row+=1) {
             col=2-row;
-            //index = offset(row,col)
             if (this.is_not_equal(row,col,color)) { match = false }
             else indices.push({row: row, column: col})
           }
@@ -78,9 +72,7 @@ $(function(){
         mark: function(row, column, color) {
           console.log("Board.mark invoked with "+row+", "+column+", "+color)
           console.log(this.board);
-          //if (this.is_valid(index)) {
           if (this.is_valid(row, column)) {
-            //this.set_cell(index, color)
             console.log("this.is_valid")
             this.set_cell(row, column, color)
             this.occupied_spaces += 1
@@ -93,7 +85,6 @@ $(function(){
           return this.is_equal(row, column, "")
         },
         
-        //
         set_cell: function(row, column, color) {
           var index = Number(row)*3 + Number(column)
           console.log("set_cell: index = "+index)
@@ -115,18 +106,17 @@ $(function(){
           return this.get_cell(row, column) !== value
         },
 
-        index2_row_col: function(index) {   // should this be private inside all_squares
-          return {
-            row:    Math.floor(index / 3),
-            column: (index % 3)
-          }
-        },
-
         all_squares: function() {
 
-// private function DRT
+          function index2_row_col(index) {
+            return {
+            row:    Math.floor(index / 3),
+            column: (index % 3)
+            }
+          }
+
           return [0,1,2,3,4,5,6,7,8].map(function(index) {
-            return Board.index2_row_col(index)
+            return index2_row_col(index)
           })
         }      
     };
@@ -141,16 +131,9 @@ $(function(){
 
         },
 
-        // each number represents a unique id in index.html which corresponds to each square
-        forEach_square: function(callback) {
-          [0,1,2,3,4,5,6,7,8].forEach( function(elem) {
-            callback(elem)
-          })
-        },
-
         reset: function() {
 
-          var template = function(row,column) { //row,column) {
+          var template = function(row,column) {
             var html_template = ""
             html_template += "<div id='r"+row+"_c"+column+"' class='square'>"
             html_template += "  <div class='content'>"
@@ -158,13 +141,11 @@ $(function(){
             html_template += "</div>"
             return html_template
           }
-
           // Note that the order of the following code is important, building the DOM must happen
           // prior to setting up the click handler. I believe there is another way to accomplish this.
           var game_board = $('#game_board');
           game_board.empty()
           GameController.all_squares().forEach(function(elem) {
-          //this.forEach_square(function (elem) {
             game_board.append(template(elem.row,elem.column))
           })
           console.log("game_board")          
@@ -174,24 +155,18 @@ $(function(){
               // id_row and col could be extracted from jQuery object 'this' downstream, but i think this is a view concern  
               var id_row = this.id.substring(1,2) // r0_c0
               var id_col = this.id.substring(4,5) // 01234       
-              //var id_index = this.id.substring(1,2)        
               GameController.mark(this,id_row,id_col)
             })                     
           }) // document.ready 
 
           if (this.is_first_time) { alert("Batman goes first!!") }
-            console.log("this is first time = "+this.is_first_time)  
-            this.is_first_time = false
-            console.log("this is first time = "+this.is_first_time)
-
-          //console.log("DRT 9/27/15")
-          //console.log(GameController.all_squares())
+          this.is_first_time = false
         },  
 
         render_square: function(e,attr,image) { 
           console.log("render invoked")         
 
-          // if image is not given, then an empty square will be returned
+          // if image is not given, then an empty square will be returned and that is ok
           var template = function(attr,image) {
             var html_template = "";
             html_template += "<div class='content'>"
@@ -207,7 +182,7 @@ $(function(){
         render_winner: function(winner_ary) {
 
           // Array.prototype.indexOf does not work with objects so we use this deep indexOf
-          //http://stackoverflow.com/questions/8668174/indexof-method-in-an-object-array
+          // http://stackoverflow.com/questions/8668174/indexof-method-in-an-object-array
           function arrayObjIndexOf(ary, elem) {
             for(var i = 0, len = ary.length; i < len; i++) {
               if (ary[i].row === elem.row && ary[i].column === elem.column) return i;
@@ -216,13 +191,11 @@ $(function(){
            }
 
           // render winner squares by fading others
-          x = GameController.all_squares().filter(function(elem) {
-            console.log(elem)
-            return arrayObjIndexOf(winner_ary,elem) === -1
-          })
-          console.log("DRT 9.27.15")
-          console.log(x)
-          x.forEach( function(elem) {
+          GameController.all_squares()
+            .filter(function(elem) {
+              return arrayObjIndexOf(winner_ary,elem) === -1
+            })
+            .forEach( function(elem) {
               $("#r"+elem.row+"_c"+elem.column+" div").addClass("fade")
           })
         }
@@ -249,12 +222,11 @@ $(function(){
        
        init: function() { 
          this.pieces = ["Batman", "Joker"] // Batman, Joker
-         // not sure if I should be storing image and attr inside of game. more of a view thing unless it 
-         // can be changed, selected by the user
+         // not sure if image and attr should be inside of game. more of a view thing unless it 
+         // can be changed, ie selected by the user
          this.images = ["images/batman_logo_final.png", "images/aa6e8d0ba1ee3a0fea3df7c7d5b00f6b.jpg"]
          this.attr   = ["class='circle'", ""] // batman is a circle, jocker is a square
          this.current_player_index = 0
-         Board.init()
        }
     };
 
@@ -264,25 +236,22 @@ $(function(){
         return Board.all_squares()
       },
 
-      //mark: function(e, index) {
       mark: function(e, row, column) {
         console.log("GameController.mark invoked")
         var current_player = Game.current_player()
         var player_wins = false
         var draw        = false
         var winner
-        //if (Board.mark(index, current_player) ) {
+
         if (Board.mark(row, column, current_player) ) {
           View.render_square(e, Game.current_attr(), Game.current_image())
           winner = Board.is_winner(current_player)
           if (winner) {
-            console.log(current_player+" wins")
             console.log(winner)
             player_wins = true;
             View.render_winner(winner)
             alert(current_player+" wins. Click OK to begin a new game.")
           } else if (Board.is_full()) {
-              console.log("Draw")
               draw = true
               alert("The game is a draw. Click OK to begin a new game")
           } else {
@@ -293,7 +262,6 @@ $(function(){
             Board.init()
             View.reset()
           }
-          console.log("Here")
         }
       },
       
